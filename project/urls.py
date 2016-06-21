b67578c74b9519e2327d184d842cc33c8ecf04f4
Hessa AlJeri <hessaaljeri@hotmail.com>
2016-06-21 18:15:40 +0300

    test

---
+++ project/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^landingpage/$', TemplateView.as_view(template_name='landingpage1.html'), name='landingpage'),
+
+    url(r'^admin/', include(admin.site.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
