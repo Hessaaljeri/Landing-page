1d7e6b4e1d92fd904d7ebb6b81c019fe9e0e1aee
Arwaal <arwa.a.aljasser@gmail.com>
2016-07-15 09:31:48 +0300

    urls improved

--- project/urls.py
+++ project/urls.py
@@ -10,12 +10,17 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
+    url(r'^$', TemplateView.as_view(template_name='landingpage1.html')),
     url(r'^landingpage/$', TemplateView.as_view(template_name='landingpage1.html'), name='landingpage'),
-
+    url(r'^email_submission/$','main.views.email_submission'),
 
     url(r'^sendmail/$', 'main.views.sendmail'),
 
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+
+
+    
+    
