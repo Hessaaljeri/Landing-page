a2c2e28a20728bcb5f42ef1f5a86d58c07e099b8
Arwaal <arwa.a.aljasser@gmail.com>
2016-07-30 22:57:03 +0300

    DB

--- project/settings.py
+++ project/settings.py
@@ -93,10 +93,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {  
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'Landing-page', 
+        'HOST': '127.0.0.1', 
+        'USER': 'root', 
+        'PASSWORD': 'root', 
+        'PORT': ''
     }
 }
 
